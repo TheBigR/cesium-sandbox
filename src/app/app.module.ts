6e7be6f016ef6b8157acadc9b37051d0f6a63b75
Roy <roy@webiks.com>
2019-01-07 15:23:03 +0200

    cesium sample works.
    do not upgrade angular-cesium from 0.53

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AngularCesiumModule, AngularCesiumWidgetsModule} from 'angular-cesium';
 import { AppComponent } from './app.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
+    AngularCesiumModule.forRoot(),
+    AngularCesiumWidgetsModule,
     BrowserModule
   ],
   providers: [],
