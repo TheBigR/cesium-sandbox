42479c87033f487e6e75dde63a177715643459c2
Roy <roy@webiks.com>
2019-03-11 09:29:41 +0200

    map provider component removed. two divs set on app.component. two maps are on the divs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,18 @@ import { NgModule } from '@angular/core';
 import {AngularCesiumModule, AngularCesiumWidgetsModule, ViewerConfiguration} from 'angular-cesium';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
-import { MapProviderComponent } from './components/map-provider/map-provider.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent,
-    MapProviderComponent
+    MapComponent
   ],
   imports: [
+    AngularCesiumModule,
     AngularCesiumModule.forRoot(),
     AngularCesiumWidgetsModule,
+    FormsModule,
     BrowserModule
   ],
   providers: [
--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
-import {MapLayerProviderOptions, MapsManagerService} from 'angular-cesium';
+import {MapLayerProviderOptions, MapsManagerService, ViewerConfiguration} from 'angular-cesium';
 
 @Component({
   selector: 'app-map',
@@ -9,9 +9,44 @@ import {MapLayerProviderOptions, MapsManagerService} from 'angular-cesium';
 })
 
 export class MapComponent implements OnInit, OnDestroy {
+  viewer;
+  mapLayerProviderOptions = MapLayerProviderOptions;
 
-  constructor( ) {
+  ionToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhMWY3MDBjYi1hM2NmLTRiY2YtYTM3My1mMTk5MjFiYTg5NDUiLCJpZ' +
+    'CI6NjU0Miwic2NvcGVzIjpbImFzbCIsImFzciIsImFzdyIsImdjIl0sImFzc2V0cyI6WzIsM10sImlhdCI6MTU1MTM0NzUzNH0.iLuDmwFBzHKNiEIoYBakVCiwNS' +
+    '1xZyen-2GJAZ9fjJk';
 
+  providerOptions = {
+    url: 'https://dev.virtualearth.net',
+    key: 'AmJ0O28GqMP3Dh1xqajFAFMuKb9f0YCXtGWJ5G4NU_PeE899SYup3ngALmDschnu',
+    DEFAULT_VIEW_FACTOR: 0,
+    baseLayerPicker: false
+  };
+
+  viewerConfigurationOptions = {
+    skyBox: false,
+    skyAtmosphere: false,
+    contextOptions: {
+      webgl: {
+        alpha: true
+      }
+    }
+  };
+
+  maps = [
+    {id: 'transMap',
+     containerId: 'topOfVideo'
+    },
+    {
+      id: 'secMap',
+      containerId: 'secContainer'
+    }
+  ];
+
+  constructor( private mapsManagerService: MapsManagerService, viewerConfiguration: ViewerConfiguration) {
+    this.mapsManagerService = mapsManagerService;
+    viewerConfiguration.viewerOptions = this.viewerConfigurationOptions;
+    Cesium.Ion.defaultAccessToken = this.ionToken;
   }
 
 
