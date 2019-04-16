f29f55419c4a5f22a7bc6e4bbbe4efc4baa63232
TheBigR <nevoroy@gmail.com>
2019-04-16 19:03:42 +0300

    toolbar embedded and working.

--- src/app/components/map/map-toolbar/map-toolbar.component.ts
+++ src/app/components/map/map-toolbar/map-toolbar.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  CameraService,
+  CesiumService,
+  CirclesEditorService,
+  EllipsesEditorService,
+  HippodromeEditorService,
+  PolygonsEditorService,
+  PolylineEditorObservable,
+  RangeAndBearingComponent,
+  ZoomToRectangleService
+} from 'angular-cesium';
 
 @Component({
   selector: 'app-map-toolbar',
   templateUrl: './map-toolbar.component.html',
-  styleUrls: ['./map-toolbar.component.css']
+  styleUrls: ['./map-toolbar.component.css'],
+  providers: [ZoomToRectangleService, CirclesEditorService, EllipsesEditorService, PolygonsEditorService, HippodromeEditorService],
 })
 export class MapToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor( private cameraService: CameraService,
+               private zoomToRectangleService: ZoomToRectangleService,
+               private cesiumService: CesiumService,
+               private circlesEditor: CirclesEditorService,
+               private ellipsesEditor: EllipsesEditorService,
+               private polygonsEditor: PolygonsEditorService,
+  ) {
+    this.zoomToRectangleService.init(cesiumService, cameraService);
+  }
 
   ngOnInit() {
   }
 
+  drawCircle() {
+    this.circlesEditor.create();
+  }
+
+  drawEllipse() {
+    this.ellipsesEditor.create();
+  }
+
+  drawPolygon() {
+    this.polygonsEditor.create();
+  }
 }
